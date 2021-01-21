9a123516c7e69dfcebf1d8d7cae4ae4f86f23e61
xyhuang1116 <xyhuang1116@gmail.com>
2021-01-21 22:28:43 +0100

    Complete blob&BranchManager class and init/add/rm command

--- Xinying/Proj2/gitlet/Main.java
+++ Xinying/Proj2/gitlet/Main.java
@@ -1,5 +1,8 @@
 package gitlet;
 
+import java.io.File;
+import java.io.IOException;
+
 /** Driver class for Gitlet, the tiny stupid version-control system.
  *  @author
  */
@@ -7,8 +10,33 @@ public class Main {
 
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND> .... */
-    public static void main(String... args) {
+    public static void main(String... args) throws IOException {
         // FILL THIS IN
+        String workingDirectory;
+        workingDirectory = System.getProperty("user.dir");
+
+        Gitlet gitletRe = new Gitlet(workingDirectory);
+
+        // if args[0] == XXX do XXX  init
+        if(args.length >0){
+            switch(args[0]){
+                case "init":
+                    if(args.length > 1){
+                        System.out.println("Incorrect operands.");
+                    }else{gitletRe.init();}
+                case "commit":
+                    // try to construct a new commit from gitlet packge
+
+                default:
+                    System.out.println("No command with that name exists.");
+            }
+        }else{
+            System.out.println("Please enter a command.");
+        }
+
     }
+
+
+
 
 }
